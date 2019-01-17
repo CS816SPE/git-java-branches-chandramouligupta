15a213e8afe1a9cf43c86ee51ea5ca7e5fcbf611
Chandramouli <chandramouligupta50@gmail.com>
2019-01-17 12:09:45 +0530

    Multipication Code

--- demo.cpp
+++ demo.cpp
@@ -2,15 +2,8 @@
 
 using namespace std;
 
-int add(int a,int b);
 int multiply(int a,int b);
 
-int main(){
-	int a,b;
-	cin>>a>>b;
-	return 0;
-}
-
-int add(int a,int b){
-	return a+b;
+int multiply(int a,int b){
+	return a*b;
 }
